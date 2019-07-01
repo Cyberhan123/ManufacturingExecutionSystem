b48d63113e2cbef96883ab38d3ddaf139affd763
韩瀚 <hanhan@corp.netease.com>
2019-07-01 19:15:19 +0800

    溜了溜了

--- src/main/java/cn/hselfweb/mes/arithmetic/Arithmetic.kt
+++ src/main/java/cn/hselfweb/mes/arithmetic/Arithmetic.kt
@@ -3,7 +3,5 @@ package cn.hselfweb.mes.arithmetic
 import cn.hselfweb.mes.arithmetic.pojo.ArithmeticParams
 
 abstract class Arithmetic {
-
-    var arithmeticParams: ArithmeticParams? = null
     abstract fun sort(arithmeticParams: ArithmeticParams): ArithmeticParams
 }
